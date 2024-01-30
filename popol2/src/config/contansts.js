26c62379cbfeb3595a57f062c72e1f06ac8647a8
chajeongheon <jeongheon1127@gmail.com>
2024-01-30 11:34:09 +0900

    123

--- popol2/src/config/contansts.js
+++ popol2/src/config/contansts.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:8001";
-export const API_URL = "http://www.kimjunnyoung.com/app2";
+export const API_URL = "http://3.131.43.198/app2";
 // export const API_URL = "https://lamp-shopping-server-7ingout.herokuapp.com";
